2149b917e35c868c3a88f4bbf4e9fa4c9892e036
Ryan Phillips <ryan.phillips@rackspace.com>
2011-12-30 15:24:11 -0600

    woops.. wrong bourbon file :/

--- init.lua
+++ init.lua
@@ -1,88 +1,35 @@
-local async = require 'async'
+local async = require './init.lua'
 local table = require 'table'
 local string = require 'string'
 local math = require 'math'
 
-local TS = tostring
 local fmt = string.format
 
 local checked = 0
 
--- ###########
--- # Results #
--- ###########
-
-local function msec(t)
-   if t and type(t) == "number" then 
-      return fmt(" (%.2fms)", t * 1000)
-   else
-      return ""
-   end
+local asserts = {}
+asserts.equal = function(a, b)
+  checked = checked + 1
+  assert(a == b)
 end
-
-
-local RPass = {}
-local passMT = {__index=RPass}
-function RPass:tostring_char() return "." end
-function RPass:add(s, name) s.pass[name] = self end
-function RPass:type() return "pass" end
-function RPass:tostring(name)
-   return fmt("PASS: %s%s%s",
-              name or "(unknown)", msec(self.elapsed),
-              self.msg and (": " .. tostring(self.msg)) or "")
+asserts.ok = function(a)
+  checked = checked + 1
+  assert(a)
 end
-
-
-local RFail = {}
-local failMT = {__index=RFail}
-function RFail:tostring_char() return "F" end
-function RFail:add(s, name) s.fail[name] = self end
-function RFail:type() return "fail" end
-function RFail:tostring(name)
-   return fmt("FAIL: %s%s: %s%s",
-              name or "(unknown)",
-              msec(self.elapsed), self.reason or "",
-              self.msg and (" - " .. tostring(self.msg)) or "")
+asserts.equals = function(a, b)
+  checked = checked + 1
+  assert(a == b)
 end
-
-
-local RSkip = {}
-local skipMT = {__index=RSkip}
-function RSkip:tostring_char() return "s" end
-function RSkip:add(s, name) s.skip[name] = self end
-function RSkip:type() return "skip" end
-function RSkip:tostring(name)
-   return fmt("SKIP: %s()%s", name or "unknown",
-              self.msg and (" - " .. tostring(self.msg)) or "")
+asserts.array_equals = function(a, b)
+  checked = checked + 1
+  assert(#a == #b)
+  for k=1, #a do
+    assert(a[k] == b[k])
+  end
 end
-
-
-local RError = {}
-local errorMT = {__index=RError}
-function RError:tostring_char() return "E" end
-function RError:add(s, name) s.err[name] = self end
-function RError:type() return "error" end
-function RError:tostring(name)
-   return self.msg or
-      fmt("ERROR (in %s%s, couldn't get traceback)",
-          msec(self.elapsed), name or "(unknown)")
-end
-
-
-local function Pass(t) return setmetatable(t or {}, passMT) end
-local function Skip(t) return setmetatable(t, skipMT) end
-local function Error(t) return setmetatable(t, errorMT) end
-local function Fail(t) return setmetatable(t, failMT) end
-
-local function wraptest(flag, msg, t)
-   checked = checked + 1
-   t.msg = msg
-   if not flag then error(Fail(t)) end
-end
-
----exp == got.
-function assert_equal(exp, got, tol, msg)
-  wraptest(exp == got, msg, { reason=fmt(""), TS(exp), TS(tol), TS(got)})
+asserts.not_nil = function(a)
+  checked = checked + 1
+  assert(a ~= nil)
 end
 
 function is_test_key(k)
@@ -104,11 +51,13 @@ local function get_tests(mod)
 end
 
 local run_test = function(runner, callback)
+  p (fmt("Running %s", runner.name))
+
   local test_baton = {}
   test_baton.done = function()
+    callback()
   end
-  runner.func(test_baton)
-  callback()
+  runner.func(test_baton, asserts)
 end
 
 local run = function(mods)
@@ -121,33 +70,17 @@ local run = function(mods)
   async.forEachSeries(runners, function(runner, callback)
     run_test(runner, callback)
   end, function(err)
-    print(fmt("Executed %s", checked))
+    if err then
+      p(err)
+      return
+    end
+    p(fmt("Executed %s asserts", checked))
   end)
 end
 
-function assert_array_equal(_a, _b)
-  for k=1,#_b do
-    print(_a[k])
-    print(_b[k])
-    assert_equal(_a[k], _b[k])
-  end
-end
-
---got ~= nil
-function assert_not_nil(got, msg)
-   wraptest(got ~= nil, msg,
-            { reason=fmt("Expected non-nil value, got %s", TS(got)) })
-end
-
-function assert_true(got, msg)
-   wraptest(got, msg, { reason=fmt("Expected success, got %s.", TS(got)) })
-end
+-- Exports
 
 local exports = {}
-exports.asserts = {}
-exports.asserts.equal = assert_equal
-exports.asserts.True = assert_true
-exports.asserts.array_equal= assert_array_equal
-exports.asserts.not_nil = assert_not_nil
+exports.asserts = asserts
 exports.run = run
 return exports
